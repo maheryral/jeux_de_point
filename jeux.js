6de934755d80155073012c80eb3c4dd04a1a5965
Mahery <maheryralaiarison@gmail.com>
2022-10-24 18:16:40 +0300

    first step ok

--- jeux.js
+++ jeux.js
@@ -33,6 +33,7 @@ var joueur1=document.getElementById('j1').innerHTML;
 var joueur2=document.getElementById('j2').innerHTML;
 var couleur='red'
 var pers='joueur1'
+var teste=false
 for (let i = 0; i < champ.length; i++) {
         
     champ[i].addEventListener('click',function () {
@@ -57,9 +58,12 @@ function func(y){
                 couleur='black'
                 y.dataset.jeux='red'
                 
-                teste=mandroso(y,y,'red',y.dataset.pd,y.dataset.nd)
+                mandroso(y,y,'red',y.dataset.pd,y.dataset.nd)
                 if (teste==true) {
                     alert('bien mena e')
+                }
+                else{
+                    // alert(teste)
                 }
                 
             }
@@ -67,9 +71,12 @@ function func(y){
                 
                 y.dataset.jeux='black'
                 couleur='red'
-                teste=mandroso(y,y,'black',y.dataset.pd,y.dataset.nd)
+                mandroso(y,y,'black',y.dataset.pd,y.dataset.nd)
                 if (teste==true) {
                     alert('bien mainty  e')
+                }
+                else{
+                    // alert(teste)
                 }
             }
 }
@@ -86,37 +93,111 @@ function func(y){
     // }
 // }
 function mandroso(init,jeux,joueur,pd,nd){
-    if (nd='d' && pd=='') {
-        const vald=parseInt(jeux.dataset.c)+1// iteration droite
+    var vall=parseInt(jeux.dataset.l)
+    var valc=parseInt(jeux.dataset.l)
+    // alert('mandroso: '+init.id+' et '+jeux.id+' et '+joueur+' et '+jeux.dataset.jeux+' et '+pd+' et '+nd)
+    if (pd=='') {
         
-        if ($(jeux.dataset.l+''+vald).dataset.jeux==joueur) {
-           
-            if ($(jeux.dataset.l+''+vald).id==init.id) {
+        if (nd=='d') {
+            
+            vall=parseInt(jeux.dataset.l)
+            valc=parseInt(jeux.dataset.c)+1// iteration droite
+            
+        }
+        else if(nd=='hd'){
+            vall=parseInt(jeux.dataset.l)-1// iteration ligne 
+            valc=parseInt(jeux.dataset.c)+1
+        }
+        else if(nd=='h'){
+            vall=parseInt(jeux.dataset.l)-1// iteration ligne 
+            valc=parseInt(jeux.dataset.c)// iteration colonne
+        }
+        else if(nd=='hg'){
+            vall=parseInt(jeux.dataset.l)-1// iteration ligne 
+            valc=parseInt(jeux.dataset.c)-1// iteration colonne
+        }
+        else if(nd=='g'){
+            vall=parseInt(jeux.dataset.l)// iteration ligne 
+            valc=parseInt(jeux.dataset.c)-1// iteration colonne
+        }
+        else if(nd=='bg'){
+            vall=parseInt(jeux.dataset.l)+1// iteration ligne 
+            valc=parseInt(jeux.dataset.c)-1// iteration colonne
+        }
+        else if(nd=='b'){
+            vall=parseInt(jeux.dataset.l)+1// iteration ligne 
+            valc=parseInt(jeux.dataset.c)// iteration colonne
+        }
+        else if (nd=='bd') {
+            vall=parseInt(jeux.dataset.l)+1// iteration ligne 
+            valc=parseInt(jeux.dataset.c)+1// iteration colonne
+        }
+        
+        // const vald=parseInt(jeux.dataset.c)+1// iteration droite
+        
+        if ($(vall+''+valc).dataset.jeux==joueur) {
+            
+            if ($(vall+''+valc).id==init.id) {
                 return true
             }
             else{
-                $(jeux.dataset.l+''+vald).dataset.pd='d'
-              $(jeux.dataset.l+''+vald).dataset.nd='d'
-              mandroso(init,$(jeux.dataset.l+''+vald),joueur,'d','d')
+                $(vall+''+valc).dataset.pd=nd
+              $(vall+''+valc).dataset.nd='d'
+              mandroso(init,$(vall+''+valc),joueur,nd,'d')
             }
            
         }
         else{
-            jeux.dataset.nd='hd'
+            
+            if (nd=='d') {
+                jeux.dataset.nd='hd'
             mandroso(init,jeux,joueur,pd,'hd')
+            }
+            else if (nd=='hd') {
+                jeux.dataset.nd='h'
+                mandroso(init,jeux,joueur,pd,'h')
+            }
+            else if (nd=='h') {
+                jeux.dataset.nd='hg'
+                mandroso(init,jeux,joueur,pd,'hg')
+            }
+            else if (nd=='hg') {
+                jeux.dataset.nd='g'
+                mandroso(init,jeux,joueur,pd,'g')
+            }
+            else if (nd=='g') {
+                jeux.dataset.nd='bg'
+                mandroso(init,jeux,joueur,pd,'bg')
+            }
+            else if (nd=='bg') {
+                jeux.dataset.nd='b'
+                mandroso(init,jeux,joueur,pd,'b')
+            }
+            else if (nd=='b') {
+                jeux.dataset.nd='bd'
+                mandroso(init,jeux,joueur,pd,'bd')
+            }
+            else if (nd=='bd') {
+                
+                teste=false
+            }
+            
         }
         
     }
     else{
        
-        if(nd='d'){
-            
+        if(nd=='d'){
+           
             const vald=parseInt(jeux.dataset.c)+1// iteration droite
-            
-            if ($(jeux.dataset.l+''+vald).dataset.jeux==joueur) {
+            if (pd=='g') {
+                jeux.dataset.nd='hd'
+                mandroso(init,jeux,joueur,pd,'hd')
+            }
+            else if ($(jeux.dataset.l+''+vald).dataset.jeux==joueur) {
                 
                 if ($(jeux.dataset.l+''+vald).id==init.id) {
-                    return true
+                    teste=true
                 }
                 else{
                     $(jeux.dataset.l+''+vald).dataset.pd='d'
@@ -130,12 +211,16 @@ function mandroso(init,jeux,joueur,pd,nd){
                 mandroso(init,jeux,joueur,pd,'hd')
             }
         }
-       else if(nd='hd'){
+       else if(nd=='hd'){
             const lvalhd=parseInt(jeux.dataset.l)-1// iteration ligne 
             const cvalhd=parseInt(jeux.dataset.c)+1// iteration colonne
-            if ($(lvalhd+''+cvalhd).dataset.jeux==joueur) {
+            if (pd=='bg') {
+                jeux.dataset.nd='h'
+                mandroso(init,jeux,joueur,pd,'h')
+            }
+            else if ($(lvalhd+''+cvalhd).dataset.jeux==joueur) {
                 if ($(lvalhd+''+cvalhd).id==init.id) {
-                    return true
+                   teste=true
                 }
                 else{
                     $(lvalhd+''+cvalhd).dataset.pd='hd'
@@ -149,12 +234,16 @@ function mandroso(init,jeux,joueur,pd,nd){
                 mandroso(init,jeux,joueur,pd,'h')
             }
         }
-        else if(nd='h'){
+        else if(nd=='h'){
             const lvalh=parseInt(jeux.dataset.l)-1// iteration ligne 
             const cvalh=parseInt(jeux.dataset.c)// iteration colonne
-            if ($(lvalh+''+cvalh).dataset.jeux==joueur) {
+            if (pd=='b') {
+                jeux.dataset.nd='hg'
+                mandroso(init,jeux,joueur,pd,'hg')
+            }
+            else if ($(lvalh+''+cvalh).dataset.jeux==joueur) {
                 if ($(lvalh+''+cvalh).id==init.id) {
-                    return true
+                    teste=true
                 }
                 else{
                     $(lvalh+''+cvalh).dataset.pd='h'
@@ -167,14 +256,20 @@ function mandroso(init,jeux,joueur,pd,nd){
                 mandroso(init,jeux,joueur,pd,'hg')
             }
         }
-        else if(nd='hg'){
+        else if(nd=='hg'){
+            
             const lvalhg=parseInt(jeux.dataset.l)-1// iteration ligne 
             const cvalhg=parseInt(jeux.dataset.c)-1// iteration colonne
-            if ($(lvalhg+''+cvalhg).dataset.jeux==joueur) {
+            if (pd=='bd') {
+                jeux.dataset.nd='g'
+                mandroso(init,jeux,joueur,pd,'g')
+            }
+            else if ($(lvalhg+''+cvalhg).dataset.jeux==joueur) {
                 if ($(lvalhg+''+cvalhg).id==init.id) {
-                    return true
+                    teste=true
                 }
                 else{
+                    
                     $(lvalhg+''+cvalhg).dataset.pd='hg'
                 $(lvalhg+''+cvalhg).dataset.nd='d'
                 mandroso(init,$(lvalhg+''+cvalhg),joueur,'hg','d')
@@ -185,12 +280,16 @@ function mandroso(init,jeux,joueur,pd,nd){
                 mandroso(init,jeux,joueur,pd,'g')
             }
         }
-        else if(nd='g'){
+        else if(nd=='g'){
             const lvalg=parseInt(jeux.dataset.l)// iteration ligne 
             const cvalg=parseInt(jeux.dataset.c)-1// iteration colonne
-            if ($(lvalg+''+cvalg).dataset.jeux==joueur) {
+            if (pd=='d') {
+                jeux.dataset.nd='bg'
+                mandroso(init,jeux,joueur,pd,'bg')
+            }
+            else if ($(lvalg+''+cvalg).dataset.jeux==joueur) {
                 if ($(lvalg+''+cvalg).id==init.id) {
-                    return true
+                    teste=true
                 }
                 else{
                     $(lvalg+''+cvalg).dataset.pd='g'
@@ -204,12 +303,16 @@ function mandroso(init,jeux,joueur,pd,nd){
                 mandroso(init,jeux,joueur,pd,'bg')
             }
         }
-        else if(nd='bg'){
+        else if(nd=='bg'){
             const lvalbg=parseInt(jeux.dataset.l)+1// iteration ligne 
             const cvalbg=parseInt(jeux.dataset.c)-1// iteration colonne
-            if ($(lvalbg+''+cvalbg).dataset.jeux==joueur) {
+            if (pd=='hd') {
+                jeux.dataset.nd='b'
+                mandroso(init,jeux,joueur,pd,'b')
+            }
+            else if ($(lvalbg+''+cvalbg).dataset.jeux==joueur) {
                 if ($(lvalbg+''+cvalbg).id==init.id) {
-                    return true
+                    teste=true
                 }
                 else{
                     $(lvalbg+''+cvalbg).dataset.pd='bg'
@@ -223,12 +326,16 @@ function mandroso(init,jeux,joueur,pd,nd){
                 mandroso(init,jeux,joueur,pd,'b')
             }
         }
-        else if(nd='b'){
+        else if(nd=='b'){
             const lvalb=parseInt(jeux.dataset.l)+1// iteration ligne 
             const cvalb=parseInt(jeux.dataset.c)// iteration colonne
-            if ($(lvalb+''+cvalb).dataset.jeux==joueur) {
+            if (pd=='h') {
+                jeux.dataset.nd='bd'
+                mandroso(init,jeux,joueur,pd,'bd')
+            }
+            else if ($(lvalb+''+cvalb).dataset.jeux==joueur) {
                 if ($(lvalb+''+cvalb).id==init.id) {
-                    return true
+                    teste=true
                 }
                 else{
                     $(lvalb+''+cvalb).dataset.pd='b'
@@ -241,12 +348,15 @@ function mandroso(init,jeux,joueur,pd,nd){
                 mandroso(init,jeux,joueur,pd,'bd')
             }
         }
-        else if(nd='bd'){
+        else if(nd=='bd'){
             const lvalbd=parseInt(jeux.dataset.l)+1// iteration ligne 
             const cvalbd=parseInt(jeux.dataset.c)+1// iteration colonne
-            if ($(lvalbd+''+cvalbd).dataset.jeux==joueur) {
+            if (pd=='hg') {
+                miverina(init,jeux,joueur,pd,nd)
+            }
+            else if ($(lvalbd+''+cvalbd).dataset.jeux==joueur) {
                 if ($(lvalbd+''+cvalbd).id==init.id) {
-                    return true
+                    teste=true
                 }
                 else{
                     $(lvalbd+''+cvalbd).dataset.pd='bd'
@@ -256,8 +366,8 @@ function mandroso(init,jeux,joueur,pd,nd){
                 
             }
             else{
-                if (pd='') {
-                    return false
+                if (pd=='') {
+                    teste=false
                 }
                 else {
                     miverina(init,jeux,joueur,pd,nd)
@@ -268,49 +378,51 @@ function mandroso(init,jeux,joueur,pd,nd){
     
 }
 function miverina(init,jeux,joueur,pd,nd){
-    if (pd='d') {
+    // alert('miverina: '+pd+' et ')
+    if (pd=='d') {
         const lpd=parseInt(jeux.dataset.l)// iteration ligne 
         const cpd=parseInt(jeux.dataset.c)-1// iteration colonne
         $(lpd+''+cpd).dataset.nd='hd'
+        alert($(lpd+''+cpd).dataset.pd)
         mandroso(init,$(lpd+''+cpd),joueur,$(lpd+''+cpd).dataset.pd,'hd')
     }
-    else if(pd='hd'){
-        const lphd=parseInt(jeux.dataset.l)-1// iteration ligne 
-        const cphd=parseInt(jeux.dataset.c)+1// iteration colonne
+    else if(pd=='hd'){
+        const lphd=parseInt(jeux.dataset.l)+1// iteration ligne 
+        const cphd=parseInt(jeux.dataset.c)-1// iteration colonne
         $(lphd+''+cphd).dataset.nd='h'
         mandroso(init,$(lphd+''+cphd),joueur,$(lphd+''+cphd).dataset.pd,'h')
     }
-    else if(pd='h'){
-        const lph=parseInt(jeux.dataset.l)-1// iteration ligne 
+    else if(pd=='h'){
+        const lph=parseInt(jeux.dataset.l)+1// iteration ligne 
         const cph=parseInt(jeux.dataset.c)// iteration colonne
         $(lph+''+cph).dataset.nd='hg'
         mandroso(init,$(lph+''+cph),joueur,$(lph+''+cph).dataset.pd,'hg')
     }
-    else if(pd='hg'){
-        const lphg=parseInt(jeux.dataset.l)// iteration ligne 
-        const cphg=parseInt(jeux.dataset.c)-1// iteration colonne
+    else if(pd=='hg'){
+        const lphg=parseInt(jeux.dataset.l)+1// iteration ligne 
+        const cphg=parseInt(jeux.dataset.c)+1// iteration colonne
         $(lphg+''+cphg).dataset.nd='g'
         mandroso(init,$(lphg+''+cphg),joueur,$(lphg+''+cphg).dataset.pd,'g')
     }
-    else if(pd='g'){
+    else if(pd=='g'){
         const lpg=parseInt(jeux.dataset.l)// iteration ligne 
-        const cpg=parseInt(jeux.dataset.c)-1// iteration colonne
+        const cpg=parseInt(jeux.dataset.c)+1// iteration colonne
         $(lpg+''+cpg).dataset.nd='bg'
         mandroso(init,$(lpg+''+cpg),joueur,$(lpg+''+cpg).dataset.pd,'bg')
     }
-    else if(pd='bg'){
+    else if(pd=='bg'){
         const lpbg=parseInt(jeux.dataset.l)-1// iteration ligne 
         const cpbg=parseInt(jeux.dataset.c)+1// iteration colonne
         $(lpbg+''+cpbg).dataset.nd='b'
         mandroso(init,$(lpbg+''+cpbg),joueur,$(lpbg+''+cpbg).dataset.pd,'b')
     }
-    else if(pd='b'){
+    else if(pd=='b'){
         const lpb=parseInt(jeux.dataset.l)-1// iteration ligne 
         const cpb=parseInt(jeux.dataset.c)// iteration colonne
         $(lpb+''+cpb).dataset.nd='bd'
         mandroso(init,$(lpb+''+cpb),joueur,$(lpb+''+cpb).dataset.pd,'bd')
     }
-    else if(pd='bd'){
+    else if(pd=='bd'){
         const lpbd=parseInt(jeux.dataset.l)-1// iteration ligne 
         const cpbd=parseInt(jeux.dataset.c)-1// iteration colonne
         
